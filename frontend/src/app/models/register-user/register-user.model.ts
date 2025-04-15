891ead3d55354c34cd771ac0ff6f4a561b373f29
nachoeg <nachoeg99@gmail.com>
2025-04-15 14:07:50 -0300

    Actualizo modelo register-user

--- frontend/src/app/models/register-user/register-user.model.ts
+++ frontend/src/app/models/register-user/register-user.model.ts
@@ -1,2 +1,11 @@
 export class RegisterUser {
+  email: string;
+  password: string;
+  phone: string;
+
+  constructor(email: string, password: string, phone: string) {
+    this.email = email;
+    this.password = password;
+    this.phone = phone;
+  }
 }
